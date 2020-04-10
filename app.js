2612d5b1c491178e647cd877560aabdd65a31768
konstantin0s <constantintofan85@gmail.com>
2020-04-10 19:08:18 +0200

    Add routes for a list of pokemons

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
 const express = require('express');
 const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
-var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+var app = express();
+
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-// app.get('/beers', (req, res, next) => {
-//   punkAPI.getBeers()
-//   .then(beers => {
-//     console.log(beers[0])
-//     res.render('beers', {beers})
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   //  res.render('page500')
-//   })
-// });
-
-
-app.get('/pokemons', (req, res, next) => {
-  res.render('pokemons',
-  //  {pokemons}
-   );
-});
-
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.get('/randomPokemon', (req, res, next) => {
-  res.render('randomPokemon', {
-    // pokemon: pokemons[0];
-  })
-});
+//enables cors
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
-/*
-app.get('/randomBeer', (req,res) => {
-  punkAPI.getRandom()
-  .then(beers => {
-    res.render('randomBeer', {
-      beer: beers[0]
-    })
-  })
-})
-*/
-app.listen(3000, ()=> {
-  console.log("listening");
-});
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+var pokemons = require('./routes/pokemons');
+  app.use('/', pokemons);
+
+  // catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    res.status(err.status || 500);
+  });
+
+  app.listen(process.env.PORT || 4001, function() {
+    console.log("Server started on port 4001 :)");
+  });
+  
+  
+  module.exports = app;
---
+++ routes/pokemons.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+
+var apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/pokemons', (req, res, next) => {
+     axios.get(`${apiUrl}`).then(function (response) {
+
+    var pokemons = response.data;
+    // res.json(pokemons);
+    console.log(pokemons)
+    res.render('pokemons', {pokemons})
+  })
+});
+
+
+
+router.get('/randomPokemon', (req, res, next) => {
+      apiUrl.getRandom()
+  .then(pokemons => {
+    res.render('randomPokemon', {
+      pokemon: pokemons[0]
+    })
+  })
+//   res.render('randomPokemon', {
+//     // pokemon: pokemons[0];
+//   })
+});
+
+/*
+app.get('/randomBeer', (req,res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeer', {
+      beer: beers[0]
+    })
+  })
+})
+*/
+
+module.exports = router;
